a1237b677ffda3610400f38aedb2d02a31f89ce6
Mr VIP Mark <smartkenya254@gmail.com>
2025-07-01 23:15:30 +0300

    Add Monetag meta tag in _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,15 @@
-import "@/styles/globals.css";
+// pages/_app.js
+import Head from 'next/head';
+import '../styles/globals.css';
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+export default function MyApp({ Component, pageProps }) {
+  return (
+    <>
+      <Head>
+        {/* Monetag / PropellerAds verification */}
+        <meta name="monetag" content="b077c7db1cc69dfdcaa70a6eb9e499fe" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
