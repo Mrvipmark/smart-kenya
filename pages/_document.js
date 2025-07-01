57ecd57c451e65339bb0a5fee36f0281d8b9f734
Mr VIP Mark <smartkenya254@gmail.com>
2025-07-01 23:06:53 +0300

    Add Monetag verification meta tag

--- pages/_document.js
+++ pages/_document.js
@@ -1,9 +1,13 @@
-import { Html, Head, Main, NextScript } from "next/document";
+// pages/_document.js
+import { Html, Head, Main, NextScript } from 'next/document';
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        {/* Monetag / PropellerAds verification tag */}
+        <meta name="monetag" content="b077c7db1cc69dfdcaa70a6eb9e499fe" />
+      </Head>
       <body>
         <Main />
         <NextScript />
