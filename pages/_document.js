5725d638549bb6780786081a127e903889f807dd
Mr VIP Mark <smartkenya254@gmail.com>
2025-07-01 23:37:58 +0300

    Move Monetag meta tag to _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -3,7 +3,9 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <meta name="monetag" content="b077c7db1cc69dfdcaa70a6eb9e499fe" />
+      </Head>
       <body>
         <Main />
         <NextScript />
